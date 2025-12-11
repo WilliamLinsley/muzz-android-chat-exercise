59e70ce49a43f229e5e343adfe66aebb3101c501
WilliamLinsley <71442795+WilliamLinsley@users.noreply.github.com>
2025-12-11 22:16:02 +0000

    Added preview for message input

--- app/src/main/java/com/github/williamlinsley/muzzchat/ui/chat/components/MessageList.kt
+++ app/src/main/java/com/github/williamlinsley/muzzchat/ui/chat/components/MessageList.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.github.williamlinsley.muzzchat.domain.model.Message
 import java.time.Duration
@@ -50,4 +51,14 @@ fun MessageList(
             }
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MessageInputPreview() {
+    MessageInputBar(
+        inputText = "",
+        onInputChanged = {},
+        onSendClicked = {}
+    )
 }
